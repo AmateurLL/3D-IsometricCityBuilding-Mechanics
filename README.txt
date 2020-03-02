367d02f4bf86569386fcdce15cf0156998e39a14
AmateurLL <Amateurlslnz@gmail.com>
2020-03-02 23:56:35 +1300

    Prototype added to Github

---
+++ README.txt
@@ -0,0 +1,1 @@
+3D Isometric City Building Mechanics
---
+++ Settlement Project/Assets/Scripts/Structure.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Structure : MonoBehaviour
+{
+    public int id;
+    public int cost;
+    public string structureName;
+}
